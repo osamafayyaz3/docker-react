36d9405c5dceff5b5d273fb751d27a3bd502d36d
osamafayyaz3 <osama85@my.yorku.ca>
2020-06-23 13:51:25 -0400

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello heeh
+          I was changed into feature branch
         </p>
         <a
           className="App-link"
